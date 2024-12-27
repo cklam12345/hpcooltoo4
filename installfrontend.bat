a09ff2b48d58064f430d753bfa436fe99c6b66e8
Lam <chin-keong.lam@hp.com>
2024-12-27 15:36:33 -0800

    test

--- installfrontend.bat
+++ installfrontend.bat
@@ -3,9 +3,11 @@ set "backendDir=%repoDir%\backend"
 copy .env.example .env 
 npm install 
 npm run build 
-cd %backendDir% ;
+cd %backendDir%
 pip install -U virtualenv
 python -m virtualenv venv 
-.\venv\Scripts\activate.bat 
-pip install -r requirements.txt -U 
-mkdir data
+copy .\venv\Scripts\activate.bat .\venv\Scripts\activate2.bat
+echo .\start_windows.bat >> .\venv\Scripts\activate.bat
+echo pip install -r requirements.txt -U >>  .\venv\Scripts\activate2.bat
+echo mkdir data >> .\venv\Scripts\activate2.bat
+.\venv\Scripts\activate2.bat
