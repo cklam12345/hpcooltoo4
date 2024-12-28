3ad00b6798469641bcc4619ff1d01c53eb07cdbf
Lam <chin-keong.lam@hp.com>
2024-12-27 20:29:55 -0800

    fix browser

--- backend/start_windows.bat
+++ backend/start_windows.bat
@@ -30,5 +30,5 @@ IF "%WEBUI_SECRET_KEY%%WEBUI_JWT_SECRET_KEY%" == " " (
 
 :: Execute uvicorn
 SET "WEBUI_SECRET_KEY=%WEBUI_SECRET_KEY%"
-uvicorn open_webui.main:app --host "%HOST%" --port "%PORT%" --forwarded-allow-ips '*'
 start http://localhost:8080
+uvicorn open_webui.main:app --host "%HOST%" --port "%PORT%" --forwarded-allow-ips '*'
