eb692454dd3da82c2223c8d04ae8902fa6f9e3d8
Lam <chin-keong.lam@hp.com>
2024-12-28 16:56:01 -0800

    fix activate for ssl

--- runfrontend_ssl.bat
+++ runfrontend_ssl.bat
@@ -1,5 +1,5 @@
 set "repoDir=%UserProfile%\Documents\installcool\hpcooltoo4"
 set "backendDir=%repoDir%\backend"
 cd %backendDir%
-.\venv\Scripts\activate.bat 
+.\venv\Scripts\activate_ssl.bat 
 .\start_windows_ssl.bat
