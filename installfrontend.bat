2693dc7d812be347c82e1347741f687975c754ba
Lam <chin-keong.lam@hp.com>
2024-12-31 22:56:57 -0800

    fix install bug for repeat install

--- installfrontend.bat
+++ installfrontend.bat
@@ -7,8 +7,9 @@ cd %backendDir%
 pip install -U virtualenv
 python -m virtualenv venv 
 copy .\venv\Scripts\activate.bat .\venv\Scripts\activate2.bat
+copy .\venv\Scripts\activate.bat .\venv\Scripts\activate3.bat
 copy .\venv\Scripts\activate.bat .\venv\Scripts\activate_ssl.bat
-echo .\start_windows.bat >> .\venv\Scripts\activate.bat
+echo .\start_windows.bat >> .\venv\Scripts\activate3.bat
 echo .\start_windows_ssl.bat >> .\venv\Scripts\activate_ssl.bat
 echo pip install -r requirements.txt -U >>  .\venv\Scripts\activate2.bat
 echo mkdir data >> .\venv\Scripts\activate2.bat
