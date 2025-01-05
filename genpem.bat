68d69647077fc818e9dca3c6b3fe09bfe5c54085
Lam <chin-keong.lam@hp.com>
2025-01-05 13:44:38 -0800

    added new pem key

--- genpem.bat
+++ genpem.bat
@@ -1,3 +1,2 @@
 $env:OPENSSL_CONF ="C:\Program Files\Common Files\FireDaemon SSL 3\openssl.cnf"
-openssl genrsa -out cool.key 2048
-openssl req -x509 -newkey rsa:2048 -keyout cool.key -out cool.pem -days 365
+openssl req -x509 -newkey rsa:4096 -keyout cool.key -out cool.pem -days 10000 -nodes
