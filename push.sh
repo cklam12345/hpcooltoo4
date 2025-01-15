c1b4fb22c79571f1410f6347f19560520ee3ebac
chin-keong.lam@hp.com <edmond.xue@gmail.com>
2025-01-15 01:00:49 -0600

    fic linux

---
+++ push.sh
@@ -0,0 +1,4 @@
+docker commit 22175ff736e2 coolui_linux:version0.2
+docker tag  coolui_linux:version0.2 chinkeonglam12345/coolui_linux:version0.2
+docker login
+docker push chinkeonglam12345/coolui_linux:version0.2
